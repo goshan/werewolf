4b01ca7611726aee244e56aee5c2e6d32ded6050
azrle <azrlew@gmail.com>
2019-01-26 18:16:54 +0900

    add 24h timestamp to deal history

--- app/engins/entities/deal.rb
+++ app/engins/entities/deal.rb
@@ -1,9 +1,16 @@
 class Deal < CacheRecord
-  attr_accessor :user_id, :history
+  attr_accessor :user_id, :history, :create_at
 
   def initialize(user_id)
     self.user_id = user_id
     self.history = []
+    self.create_at = Time.now.to_i
+  end
+
+  def self.find_by_key(key)
+    deal = super(key)
+    return deal if deal && Time.now.to_i - deal.create_at <= 24*3600
+    return nil
   end
 
   def self.key_attr
@@ -28,12 +35,8 @@ class Deal < CacheRecord
   def self.clear!
     Deal.find_all.each do |deal|
       deal.history = []
+      deal.create_at = Time.now.to_i
       deal.save!
     end
-  end
-
-  def self.reset!
-    self.clear!
-    self.init!
   end
 end
