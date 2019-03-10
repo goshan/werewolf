23a7a8f2cd1b20821c9fe62e6a4950d54cd321ba
azrle <azrlew@gmail.com>
2019-03-10 16:53:28 +0900

    change schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180523085934) do
+ActiveRecord::Schema.define(version: 20190310071442) do
+
+  create_table "battle_results", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id",                    null: false
+    t.integer  "role",                       null: false
+    t.boolean  "win",        default: false, null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.index ["user_id"], name: "index_battle_results_on_user_id", using: :btree
+  end
 
   create_table "results", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id",                    null: false
