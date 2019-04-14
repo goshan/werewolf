f6e53c6f164db76bbfa7ade4cdd0488be60d3a5d
goshan <goshan.hanqiu@gmail.com>
2019-04-14 01:53:12 +0000

    fixed

--- app/games/skills/skill_response/skill_finished_response.rb
+++ app/games/skills/skill_response/skill_finished_response.rb
@@ -14,7 +14,7 @@ class SkillFinishedResponse
 
   def initialize(action, msg)
     @action = action
-    if action == ACTION_PLAY_ADUIO
+    if action == ACTION_PLAY_AUDIO
     elsif action == ACTION_SKILL_IN_DAY
       super 'alert', msg
     end
