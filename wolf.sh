31e7fed83dcc400a8cd87ff63e7621a3cde667e9
goshan <goshan.hanqiu@gmail.com>
2026-04-03 08:37:35 +0000

    env setup

--- wolf.sh
+++ wolf.sh
@@ -1,8 +1,8 @@
-if [ $1 = "release" ]; then
+if [[ $1 = "release" ]]; then
   rm -f ./public/assets/*
   RAILS_ENV=production bundle exec rake assets:precompile
-elif [ $1 = "start" ]; then
-  env_path="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)/env"
+elif [[ $1 = "start" ]]; then
+  env_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/env"
 
   if [[ ! -f "$env_path" ]]; then
     echo "Error: env not found. Please create env file from env.example"
